3a363a677e04d8c680de3264372d751482bc4c38
muwoo <2424880409@qq.com>
2021-08-05 21:07:58 +0800

    ref：init

--- preload.js
+++ preload.js
@@ -1,18 +1,3 @@
-const { readFileSync } = require('fs');
-
-window.readConfig = async function () {
-  const path = await utools.getPath('home');
-  console.log(path);
-}
-window.hideMainWindow = function () {
-  utools.hideMainWindow()
-}
 window.showNotification = function () {
-  utools.showNotification('HI, rubick')
+  rubick.showNotification('HI, rubick')
 }
-utools.onPluginReady((e) => {
-  console.log('ready', e)
-})
-utools.onPluginEnter((e) => {
-  console.log('enter', e)
-})
